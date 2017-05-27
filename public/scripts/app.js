a0109220e7f0679156d39d8d37b7cef5b6f3a1e1
bschuetzle <bill_schuetzle@sbcglobal.net>
2017-05-26 20:00:29 -0700

    added display and clear buttons for testing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,15 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  console.log("Document is ready!")
+  console.log("Document is ready, JQuery is working!")
+
+  $(".clear").on("click", function() {
+    console.log("clear button clicked");
+    $(".results").empty();
+  });
+
+  $(".display").on("click", function() {
+    console.log("display button clicked");
+  });
 
 });
