d8d020b3e39afc99886cd91801d6cc2da344f816
alyshae <alison.walas@gmail.com>
2017-05-27 11:43:25 -0700

    created seed data for 1 duck and all WDI38 students. Also added a title in index.html so the browser tab doesn't say, blank, anymore.

--- seed.js
+++ seed.js
@@ -13,3 +13,65 @@ db.Duck.create(new_duck, function(err, duck){
   console.log("Created new duck", duck._id)
   process.exit(); // we're all done! Exit the program.
 });
+
+var duckList = [{
+  name: "Princess Sadness",
+  bff: "Aly",
+  description: "While her pretty, pink crown and her accessories say 'it-girl', her eternally down-turned gaze should make clear, there's more to this fancy, feathered, flock-popular girl than meets the eye. 'P.S.' as most call her, is emotional and intellectual. She is reliable and loyal.",
+  favQuote: "You've got a friend in me.",
+  celebrityDoppleganger: "Anna Chlumsky",
+}];
+
+
+var studentList = [{
+  name: "Aly",
+  cohort: "WDI38"
+},
+{
+  name: "Ryan",
+  cohort: "WDI38"
+},
+{
+  name: "Shridar",
+  cohort: "WDI38"
+},
+{
+  name: "John",
+  cohort: "WDI38"
+},
+{
+  name: "Matt",
+  cohort: "WDI38"
+},
+{
+  name: "Justin",
+  cohort: "WDI38"
+},
+{
+  name: "Bill",
+  cohort: "WDI38"
+},
+{
+  name: "Ibrahim",
+  cohort: "WDI38"
+},
+{
+  name: "Cat",
+  cohort: "WDI38"
+},
+{
+  name: "Younji",
+  cohort: "WDI38"
+},
+{
+  name: "Kabita",
+  cohort: "WDI38"
+},
+{
+  name: "Cindy",
+  cohort: "WDI38"
+},
+{
+  name: "Nuranne",
+  cohort: "WDI38"
+}];
