34d1b8dc742f834d5dff22f6371c2e41fbc60949
bschuetzle <bill_schuetzle@sbcglobal.net>
2017-05-26 18:32:24 -0700

    completed/tested express routes with sample data

--- server.js
+++ server.js
@@ -21,6 +21,29 @@ app.use(function(req, res, next) {
 
 // var db = require('./models');
 
+var things = [
+  {
+    id: 1,
+    name: "thing1",
+    description: "thing1 description"
+  },
+  {
+    id: 2,
+    name: "thing2",
+    description: "thing2 description"
+  },
+  {
+    id: 3,
+    name: "thing3",
+    description: "thing3 description"
+  },
+  {
+    id: 4,
+    name: "thing4",
+    description: "thing4 description"
+  }
+]
+
 /**********
  * ROUTES *
  **********/
@@ -47,17 +70,89 @@ app.get('/api', function apiIndex(req, res) {
   // It would be seriously overkill to save any of this to your database.
   // But you should change almost every line of this response.
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
     documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
     baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {
+        method: "GET",
+        path: "/api",
+        description: "Describes all available endpoints"
+      },
+      {
+        method: "GET",
+        path: "/api/profile",
+        description: "Data about me"
+      },
+      {
+        method: "POST",
+        path: "/api/campsites",
+        description: "E.g. Create a new campsite"
+      }
     ]
   })
 });
+
+app.get('/api/profile', function apiIndex(req, res) {
+  res.json({
+    name: "Bill Schuetzle",
+    githubUsername: "bschuetzle",
+    githubLink: "https://github.com/bschuetzle",
+    githubProfileImage: "https://avatars1.githubusercontent.com/u/27898517?v=3&s=400",
+    personalSiteLink: "",
+    currentCity: "San Francisco",
+    pets: []
+  })
+});
+
+app.get('/api/things', function index(req, res) {
+  res.json(things);
+});
+
+app.get('/api/things/:id', function show(req, res) {
+  thingID = parseInt(req.params.id);
+  console.log("thingID: ", thingID);
+  things.forEach(function(element, index) {
+    if (element.id === thingID) {
+      res.json(element);
+      return;
+    }
+  })
+});
+
+app.post('/api/things/', function create(req, res) {
+  console.log("req body: ", req.body);
+  console.log("req query: ", req.query);
+  things.push(req.query);
+  res.json(req.query);
+});
+
+app.put('/api/things/:id', function update(req, res) {
+  thingID = parseInt(req.params.id);
+  console.log("put thingID: ", thingID);
+  console.log("req query: ", req.query);
+  things.forEach(function(element, index) {
+    if (element.id === thingID) {
+      element.name = req.query.name;
+      element.description = req.query.description;
+      res.json(element);
+      return;
+    }
+  })
+});
+
+app.delete('/api/things/:id', function destroy(req, res) {
+  thingID = parseInt(req.params.id);
+  console.log("destroy thingID: ", thingID);
+  things.forEach(function(element, index) {
+    if (element.id === thingID) {
+      things.splice(index, 1);
+      res.json(element);
+      return;
+    }
+  })
+});
+
 
 /**********
  * SERVER *
