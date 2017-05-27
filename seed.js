46f23af3687a714ba47ad17a49ea9dc893d25a1f
alyshae <alison.walas@gmail.com>
2017-05-27 16:02:22 -0700

    updated duckList to include 3 ducks for seed data.

--- seed.js
+++ seed.js
@@ -7,18 +7,26 @@ var db = require('./models');
 var duckList = [{
   name: "Princess Sadness",
   bff: "Aly",
-  description: "While her pretty, pink crown and her accessories say 'it-girl', her eternally down-turned gaze should make clear, there's more to this fancy, feathered, flock-popular girl than meets the eye. 'P.S.' as most call her, is emotional and intellectual. She is reliable and loyal.",
+  description: "While her pretty, pink crown and her accessories say 'it-girl', her eternally down-turned gaze should make clear, there's more to this fancy, feathered, flock-popular girl than meets the eye. She's got a knack for problem solving.",
   favQuote: "You've got a friend in me.",
   celebrityDoppleganger: "Anna Chlumsky",
   cohort: "WDI38"
 },
 {
-  name: "Fake Duck",
-  bff: "Freddy",
-  description: "Fake duck is imaginary.",
-  favQuote: "None.",
-  celebrityDoppleganger: "None",
-  cohort: "None"
+  name: "Mortimer",
+  bff: "Michelle",
+  description: "Mortimer loves racquetball. He is rarely seen without his Champion brand cross-trainers and his plastic, protective sports-goggles. When he's not winding-up his backswing on the four-wall court, you can be sure he's not floating around the lazy-pond...Mortimer is an All-Star coach and he gets Team Michelle across the finish line every time!!",
+  favQuote: "The ball is in your court, now go score!",
+  celebrityDoppleganger: "Chris Kattan",
+  cohort: "Many"
+},
+{
+  name: "Orren",
+  bff: "Nathan",
+  description: "TBD.",
+  favQuote: "TBD.",
+  celebrityDoppleganger: "Rowan Atkinson",
+  cohort: "Many"
 }];
 
 
