2d3d756e9ffc504ac00e36198c8ae5bd3b74f981
alyshae <alison.walas@gmail.com>
2017-05-27 12:01:21 -0700

    added initial ajax request to app.js and a test function to try and console.log my ducks list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,15 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
+  $.ajax({
+    method: 'GET',
+    url: '/api/ducks',
+    success: handleSuccess,
+    error: handleError
+  });
 
 });
+
+function getAllDucks(ducks) {
+  return console.log("Ducky Listing Test: ", ducks);
+}
