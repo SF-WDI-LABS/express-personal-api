f419888b57c23b8ea377fcca8bcf7537f3f1b59e
drstrangele <chickenle@gmail.com>
2017-05-28 00:55:55 -0700

    created sammich.js and sammichSchema

---
+++ models/sammich.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+// mongoose.Promise = global.Promise;
+
+var sammichSchema = new Schema({
+  resturantName: String,
+  fried: Boolean,
+  description: String,
+  address: String,
+  coordinates: String,
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+var Sammich = mongoose.model("Sammich", sammichSchema);
+
+module.exports = Sammmich;
