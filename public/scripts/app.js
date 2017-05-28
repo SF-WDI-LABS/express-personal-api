9e7944fadd2565fd5c97f0a31b7fa0220dc8ab12
alyshae <alison.walas@gmail.com>
2017-05-27 17:09:29 -0700

    Added quick HTML form for testing. Working on app.js functions to try and render new duck onto page.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
 console.log("Sanity Check: JS is working!");
 
+var $duckiesArray;
+var allDucks = [];
+var newDuckArray = [];
+
 $(document).ready(function(){
+
+  $duckiesArray = $('#ducksTarget');
 
   $.ajax({
     method: 'GET',
@@ -9,11 +15,30 @@ $(document).ready(function(){
     //error: handleError
   });
 
+  $('#newDuckForm').on('submit', function(el) {
+    el.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: '/api/ducks',
+      data: $(this).serialize(),
+      success: newDuckSuccess,
+      //error: newDuckError
+    });
+
 });
 
 function getAllDucks(ducks) {
   console.log("Ducky Listing Test: ", ducks);
   ducks.forEach(function(d){
-    $('#ducks_target').append(`<li>${d.name} belongs to ${d.bff}. ${d.description} Her/His favorite quote is '${d.favQuote}'</li>`);
+    $('#ducksTarget').append(`<li>${d.name} belongs to ${d.bff}. ${d.description} Her/His favorite quote is '${d.favQuote}'</li>`);
   });
 };
+
+
+
+function newDuckSuccess(jsonDuck) {
+  $('#newDuckForm input').val('');
+  newDuckArray.push(jsonDuck);
+
+  getAllDucks(newDuckArray);
+}
