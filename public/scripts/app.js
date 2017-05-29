510fc5fb33f5bf721c0a45127a7880bc0108edff
alyshae <alison.walas@gmail.com>
2017-05-29 12:40:27 -0700

    appended HTML is a mess but functional. Need to move on & create the additional functions to add, delete & then hopefully update.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,6 @@ console.log("Sanity Check: JS is working!");
 
 var $duckiesArray;
 var allDucks = [];
-var newDuckArray = [];
 
 $(document).ready(function(){
 
@@ -11,36 +10,53 @@ $(document).ready(function(){
   $.ajax({
     method: 'GET',
     url: '/api/ducks',
-    success: getAllDucks,
+    success: handleSuccess,
     //error: handleError
   });
 
-  // $('#newDuckForm').on('submit', function(el) {
-  //   el.preventDefault();
-  //   $.ajax({
-  //     method: 'POST',
-  //     url: '/api/ducks',
-  //     data: $(this).serialize(),
-  //     success: newDuckSuccess,
-  //     //error: newDuckError
-  //   });
-
 });
 
-function getAllDucks(ducks) {
-  console.log("Ducky Listing Test: ", ducks);
-  ducks.forEach(function(d){
-    let g;
-    d.gender === "Female" ? g = ["She", "Her"] : g = ["He", "His"];
-    $('#ducksTarget').append(`<li>${d.name} belongs to ${d.bff}. ${d.description} ${g[1]} favorite quote is '${d.favQuote}'</li>`);
-  });
+
+
+// get duck formatted into HTML elements
+function getDuckHTML(d) {
+  let g;
+  d.gender === "Female" ? g = ["She", "Her"] : g = ["He", "His"];
+  return
+  `<li>${d.name} belongs to ${d.bff}. ${d.description} ${g[1]} favorite quote is '${d.favQuote}'</li>
+  <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id=${d._id}>Delete Duck</button>`;
 };
 
+// get all ducks in given array formatted into HTML elements
+function getAllDucksHTML(ducks) {
 
+  let result = ducks.forEach(function(d){
+    let g;
+    d.gender === "Female" ? g = ["She", "Her"] : g = ["He", "His"];
+    $('#ducksTarget').append(
+      `<div class="row>">
+        <div class="col-xs-8-offset-1">
+          <li>${d.name} belongs to ${d.bff}. ${d.description} ${g[1]} favorite quote is '${d.favQuote}'</li>
+        </div>
+        <div class="col-xs-3">
+          <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id=${d._id}>Delete Duck</button>
+        </div>
+      </div><br/>`
+    );
+  });
+  return result;
+};
 
-// function newDuckSuccess(jsonDuck) {
-//   $('#newDuckForm input').val('');
-//   newDuckArray.push(jsonDuck);
-//
-//   getAllDucks(newDuckArray);
-// }
+// append all the duck data to the HTML view - #duckTarget
+function render() {
+  $duckiesArray.empty();
+
+  let duxHTML = getAllDucksHTML(allDucks);
+
+  $duckiesArray.append(duxHTML);
+}
+
+function handleSuccess(jsonData) {
+  allDucks = jsonData;
+  render();
+};
