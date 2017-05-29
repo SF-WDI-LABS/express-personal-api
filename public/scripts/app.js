2d2351c37a0c975e6c35d92dae42896b31c519c6
younjiwoo <younjiwoo@gmail.com>
2017-05-28 21:19:16 -0700

    changed the path for url

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,9 +5,13 @@ $(document).ready(function(){
     $.ajax({
         method: 'GET',
         dataType: 'json',
-        url: 'https://agile-inlet-98335.herokuapp.com/api/',
+        url: 'https://agile-inlet-98335.herokuapp.com/api/movies',
         success: function(responseData){
-            
+            let source   = $("#handlebars-template").html();
+            let template = Handlebars.compile(source); // turn it into HTML.
+            let html = template({movies: responseData}); // put the data into HTML.
+            $('main').html(html);
+
         }
         
     })
