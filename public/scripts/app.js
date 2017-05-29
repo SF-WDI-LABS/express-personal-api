6a85c44c556cf442ed3b7c3f464b1316e71bb601
drstrangele <chickenle@gmail.com>
2017-05-29 15:56:11 -0700

    added google map and ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,42 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
+  let map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: new google.maps.LatLng(37.775,-122.419),
+      zoom: 2
+    });
+  }
+
+  initMap();
 
 });
+
+
+// $('button').on(click, function(event){
+
+  $.ajax({
+     method: "GET",
+     url: "/api/sammich",
+     success: function(sammiches){
+       console.log("success")
+
+       sammiches.forEach(function(sammich){
+         $(".sammich-list").append(`
+           <div class="sammich" id="sammich-${sammich._id}">
+             <h3>Chicken Spots:</h3>
+             <h4>${sammich.resturantName}</h4>
+             <img src=${sammich.image}>
+             <p>Description: ${sammich.description}</p>
+             <p>Address: ${sammich.address}</p>
+           </div>
+         `);
+       })
+
+     },
+     error: function(){
+       alert("Meh. It didnt work.")
+     }
+   });
+// });
