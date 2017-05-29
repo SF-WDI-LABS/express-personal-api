0405ae4d3f31d8449ad3190032a9baaab666fc82
younjiwoo <younjiwoo@gmail.com>
2017-05-28 21:54:43 -0700

    added show one movie event listener

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,9 +11,25 @@ $(document).ready(function(){
             let template = Handlebars.compile(source); // turn it into HTML.
             let html = template({movies: responseData}); // put the data into HTML.
             $('main').html(html);
-
         }
-        
     })
+    
+    $('main').on('click', 'button.show', function(e){
+        let movieId = $(e.target).attr('data-id');
+        $.ajax({
+            method: 'GET',
+            dataType: 'json',
+            url: `https://agile-inlet-98335.herokuapp.com/api/movies/${movieId}`,
+            success: function(responseData){
+                let source   = $("#one-movie-template").html();
+                let template = Handlebars.compile(source); // turn it into HTML.
+                let html = template(responseData); // put the data into HTML.
+                $('main').html(html);
+            }
+        })
+    })
+    
+    
+    
 
 });
