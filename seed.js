4f6058a6f7eb378703e2d1e010100adca914f393
younjiwoo <younjiwoo@gmail.com>
2017-05-28 21:51:14 -0700

    changed ampersand error

--- seed.js
+++ seed.js
@@ -17,7 +17,7 @@
 let movie_listings = [
     {
         title: 'Logan',
-        genre: ['Action &amp; Adventure', 'Drama', 'Science &amp; Fiction', 'Fantasy'],
+        genre: ['Action & Adventure', 'Drama', 'Science & Fiction', 'Fantasy'],
         tomatoMeter: 92,
         haveIseenIt: false, 
         image: 'https://resizing.flixster.com/0E6Et1Fi6wFzN9PFWdZdyIl2H_c=/206x305/v1.bTsxMjMwNDQ4NDtqOzE3MzMyOzEyMDA7NjI2OzkyNA'
@@ -31,21 +31,21 @@ let movie_listings = [
     },
     {
         title: 'The Lego Batman Movie',
-        genre: ['Action & Adventure', 'Animation', 'Comedy', 'Kids &amp; Family'],
+        genre: ['Action & Adventure', 'Animation', 'Comedy', 'Kids & Family'],
         tomatoMeter: 90,
         haveIseenIt: false,
         image: 'https://resizing.flixster.com/5e1sqBSS4Yjl16MU2CK4QtFrTA8=/206x305/v1.bTsxMjI3NjU0NTtqOzE3MzMyOzEyMDA7NDA1MDs2MDAw'
     },
     {
         title: 'My Life as a Zucchini',
-        genre: ['Animation', 'Art House &amp; International', 'Kids &amp; Family'],
+        genre: ['Animation', 'Art House & International', 'Kids & Family'],
         tomatoMeter: 100,
         haveIseenIt: false,
         image: 'https://resizing.flixster.com/8Y1vg4k4vnncA05-gKZRrb3E3-o=/206x305/v1.bTsxMjMxMDIwNDtqOzE3MzMyOzEyMDA7MzY4OzUzOA'
     },
     {
         title: 'Berlin Syndrome',
-        genre: ['Art House &amp; International', 'Drama', 'Mystery &amp; Suspense'],
+        genre: ['Art House & International', 'Drama', 'Mystery & Suspense'],
         tomatoMeter: 76,
         haveIseenIt: false,
         image: 'https://resizing.flixster.com/wx-IBk9diQ5EMelmhnDSaPNDB-4=/206x305/v1.bTsxMjQwMTA0NTtqOzE3MzMzOzEyMDA7NjM5Ozk0Ng'
