026bef49d894114d3f784aa7baf8b9454818e763
nuranned <nuranned@gmail.com>
2017-05-30 06:23:30 -0700

    modified seed.js

--- seed.js
+++ seed.js
@@ -1,3 +1,42 @@
+var db = require('./models');
+
+var nationalparks_list = [
+  {
+    park: "Death Valley"
+    location: "California"
+    image: "http://i.imgur.com/aNW2rb6.jpg"
+    year_established: "1994"
+  },
+  {
+    park: "Yosemite"
+    location: "California"
+    image: "http://i.imgur.com/3gIl6Eo.jpg"
+    year_established: "1890"
+  },
+  {
+    park: "Zion"
+    location: "Utah"
+    image: "http://i.imgur.com/5toZrZM.jpg"
+    year_established: "1919"
+  }
+];
+
+db.Park.remove({}, function(err, books){
+  if(err) {
+    console.log('Error occured in removing park', err);
+  } else {
+    console.log('removed all parks');
+
+
+    db.Park.create(nationalparks_list, function(err, books){
+      if (err) { return console.log('err', err); }
+      console.log("created", nationalparks.length, "parks");
+      process.exit();
+    });
+  }
+});
+
+
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
