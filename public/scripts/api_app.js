6999463055e95cefa6b1bb82573b94929e045ca6
justinmcdavid <jmcdavid@gmail.comgit config --global user.email jmcdavid@gmail.com>
2017-05-30 12:12:10 -0700

    Dumped the complicated jQuery for a long-assed template string. Moved population call into the ajax call's scope.

--- public/scripts/api_app.js
+++ public/scripts/api_app.js
@@ -1,6 +1,9 @@
 // sanity check
-console.log("You're sane.");
 
+let debug = "asdf";
+
+
+console.log("You're sane.");
 
 $(document).ready(function() {
 
@@ -8,13 +11,14 @@ $(document).ready(function() {
   let allMushrooms = [];
 
 
-// Getting all the mushrooms
+  // Getting all the mushrooms
   $.ajax({
     method: "GET",
     url: apiUrl,
     success: function addMushrooms(data) {
       console.log('GET response:', data);
-      allMushrooms = data.mushrooms;
+      allMushrooms = data;
+      populate(allMushrooms);
     }
   })
 
@@ -28,42 +32,71 @@ $(document).ready(function() {
       data: newShroom,
       success: function onCreateSuccess(response) {
         console.log(response);
+        console.log(newShroom);
+        console.log(allMushrooms);
         allMushrooms.push(response);
       }
     });
   });
 
-console.log("1");
 
-function populate(entries){
-  for (let i = 0; i < entries.length; i++) {
+
+  function populate(entries) {
+    for (let i = 0; i < 10; i++) {
+
     // Object Values
-    let $common =      $("<h3/>").text(entries[i].commonName); // Create a h3, and insert text into it
-    let $tax =         $("<h4/>").text(entries[i].taxonomy); // Create a h4, and insert text into it
-    let $date =        entries[i].dateFound;
-    let $loc =         entries[i].locationFound;
-    let $eat =         entries[i].edibility;
-    let $cert =        entries[i].certainty;
-    let $photosrc =    entries[i].photo;
-    let $imgCont =    '<div class="col-sm-12 col-md-6 nopadding image-container"></div>'; // Image-container div
-    let $photo =      `<img class="mushroom-pic" src="${$photosrc}">`; // Image
-console.log("i");
-    // The below variable/value pairing is ugly. It's a bunch of nested divs that
-    // take a few variables, and I don't yet know of an easier way to put it all together.
-    let $edibLabel =  `<div class="edibility-label"><div class="certainty"><div class="percentage">${$cert}%</div><div class="certain">Certainty</div></div><div class="edible-poisonous">${$eat}</div>/div>`;
-    let $descrip =    '<div class="col-sm-12 col-md-6"></div>';
-    let $text =       `<p>Found at ${$loc} on ${$date}.</p>`;
+    let common = entries[i].commonName;
+    let tax = entries[i].taxonomy;
+    let dateF = entries[i].dateFound;
+    let loc = entries[i].locationFound;
+    let eat = entries[i].edibility;
+    let cert = entries[i].certainty;
+    let photosrc = entries[i].photo;
 
-    // Putting it all together
-    $('#gallery').append(
-                      $('<div/>').attr("id", "newEntry" + i)            // Create a div w/ unique ID
-                        .append(($imgCont).append($photo, $edibLabel),  // Add continer div, image, and label divs
-                                ($descrip.append($common, $tax, $text)) // Add h3, h4, and paragraph of
-                               )
-                      )
-  }
-};
-
-populate(allMushrooms);
-
+    function template(){
+      return `<div class="entry">
+        <div class="col-sm-12 col-md-6 nopadding image-container">
+          <img class="mushroom-pic" src="${photosrc}" />
+          <div class="edibility-label">
+            <div class="certainty">
+              <div class="percentage">
+                ${cert}
+              </div>
+              <div class="certain">
+                Certainty
+              </div>
+            </div>
+            <div class="edible-poisonous">
+              ${eat}
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <h3>${common}</h3>
+          <h4>${tax}</h4>
+          <p>
+            Found at ${loc} on ${dateF}.
+          </p>
+        </div>
+      </div>`;
+    }
+    $('#gallery').append(template);
+    }
+  };
 });
+// let $imgCont = '<div class="col-sm-12 col-md-6 nopadding image-container"></div>'; // Image-container div
+// let $photo = `<img class="mushroom-pic" src="${$photosrc}">`; // Image
+// console.log(i + " times");
+// // The below variable/value pairing is ugly. It's a bunch of nested divs that
+// // take a few variables, and I don't yet know of an easier way to put it all together.
+// let $edibLabel = `<div class="edibility-label"><div class="certainty"><div class="percentage">${$cert}%</div><div class="certain">Certainty</div></div><div class="edible-poisonous">${$eat}</div>/div>`;
+// let $descrip = '<div class="col-sm-12 col-md-6"></div>';
+// let $text = `<p>Found at ${$loc} on ${$date}.</p>`;
+
+// Putting it all together
+
+//   $('<div/>').attr("id", "newEntry" + i) // Create a div w/ unique ID
+//   .append(($imgCont).append($photo, $edibLabel), // Add continer div, image, and label divs
+//     ($descrip.append($common, $tax, $text)) // Add h3, h4, and paragraph of
+//   )
+// )
