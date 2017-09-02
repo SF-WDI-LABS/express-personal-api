011665693376cdb4d2dcfb87347c7dcf162a7c4e
Jevell Rollins <jkwr92@gmail.com>
2017-09-02 10:45:50 -0700

    wire framing complete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,14 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
 
+
+
+
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+ 
+        //initialize all modals           
+        $('.modal').modal();
+
+       $('select').material_select();
 });
--- server.js
+++ server.js
@@ -42,22 +42,7 @@ app.get('/', function homepage(req, res) {
  * JSON API Endpoints
  */
 
-app.get('/api', function apiIndex(req, res) {
-  // TODO: Document all your api endpoints below as a simple hardcoded JSON object.
-  // It would be seriously overkill to save any of this to your database.
-  // But you should change almost every line of this response.
-  res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
-    endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
-    ]
-  })
-});
+
 
 /**********
  * SERVER *
