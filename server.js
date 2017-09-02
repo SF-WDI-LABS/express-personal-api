99ecd9be6daee5a393c1f8d76cfa147eed9d822f
MaluPalu <malu.peralta808@gmail.com>
2017-09-02 16:32:10 -0700

    document

--- server.js
+++ server.js
@@ -47,14 +47,14 @@ app.get('/api', function apiIndex(req, res) {
   // It would be seriously overkill to save any of this to your database.
   // But you should change almost every line of this response.
   res.json({
-    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
+    woopsIForgotToDocumentAllMyEndpoints: false, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentationUrl: "https://github.com/MaluPalu/express-personal-api/blob/master/DOCUMENTATION.md", // CHANGE ME
+    baseUrl: "https://evening-beach-72520.herokuapp.com/", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api/cliff", description: "Cliff jumping specifiations"}, // CHANGE ME
+      {method: "POST", path: "/api/locations", description: "Location of cliff in the world"} // CHANGE ME
     ]
   })
 });
